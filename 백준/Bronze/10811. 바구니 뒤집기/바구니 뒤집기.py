5259439ef05a59118af9f79472533fb776a22e7c
DaKu00 <87750521+DaKu00@users.noreply.github.com>
2023-10-29 16:07:09 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -1,29 +1,12 @@
-num, m = map(int, input().split())
+n, m = map(int, input().split())
+n_list = [num for num in range(1, n + 1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    r_list = n_list[a - 1 : b]
+    r_list.reverse()
+    n_list = n_list[: a - 1] + r_list + n_list[b : ]
+    
+print(*n_list, sep = ' ')
 
-n = [f'{i}' for i in range(1, num + 1)]
 
-n_l = []
 
-for k in range(m):
-  i, j = map(int, input().split())
-
-  l_l = []
-
-  if i == 1 and j == num:
-    l_l = n[:]
-    l_l.reverse()
-    n = l_l
-  elif i == 1:
-    l_l = n[:j]
-    l_l.reverse()
-    n = l_l + n[j:]
-  elif j == num:
-    l_l = n[i - 1:]
-    l_l.reverse()
-    n = n[:i - 1] + l_l
-  else:
-    l_l = n[i - 1 : j]
-    l_l.reverse()
-    n = n[: i - 1] + l_l + n[j:]
-
-print(' '.join(n))
